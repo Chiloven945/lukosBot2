7b3393f1c6738940fcd2be6b3984143eef808697
ClovenBugle <yan_hun233@outlook.com>
2025-11-30 20:11:22 +0800

    feat(IpCommand): new command ip

---
+++ src/main/java/chiloven/lukosbot2/commands/IpCommand.java
@@ -0,0 +1,63 @@
+package chiloven.lukosbot2.commands;
+
+import chiloven.lukosbot2.core.CommandSource;
+import chiloven.lukosbot2.util.IpService;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class IpCommand implements BotCommand {
+    public static final IpService IS = new IpService();
+    private static final Logger log = LogManager.getLogger(IpCommand.class);
+
+    @Override
+    public String name() {
+        return "ip";
+    }
+
+    @Override
+    public String description() {
+        return "查询 IP 信息";
+    }
+
+    @Override
+    public String usage() {
+        return """
+                用法：
+                /ip <ip_address> # 查询 IP 地址，支持 v4 和 v6
+                示例：
+                /ip 1.1.1.1
+                /ip 2606:4700:4700::1111
+                """;
+    }
+
+    @Override
+    public void register(CommandDispatcher<CommandSource> dispatcher) {
+        dispatcher.register(
+                LiteralArgumentBuilder.<CommandSource>literal(name())
+                        .executes(ctx -> {
+                            ctx.getSource().reply(usage());
+                            return 1;
+                        })
+                        .then(RequiredArgumentBuilder.<CommandSource, String>argument("ip", StringArgumentType.greedyString())
+                                .executes(ctx -> {
+                                    try {
+                                        ctx.getSource().reply(
+                                                IS.getIpInfo(StringArgumentType.getString(ctx, "ip")).toString()
+                                        );
+                                        return 1;
+                                    } catch (IOException e) {
+                                        log.warn("Failed to fetch ip information.", e);
+                                        ctx.getSource().reply(e.getMessage());
+                                        return 0;
+                                    }
+                                })
+                        )
+        );
+    }
+}
---
+++ src/main/java/chiloven/lukosbot2/util/IpService.java
@@ -0,0 +1,76 @@
+package chiloven.lukosbot2.util;
+
+import com.google.gson.JsonObject;
+import org.jspecify.annotations.NonNull;
+
+import java.io.IOException;
+
+public final class IpService {
+    public static final HttpJson HJ = new HttpJson();
+    public static final JsonUtils JU = new JsonUtils();
+
+    public IpService() {
+    }
+
+    public IpInfo getIpInfo(String ip) throws IOException {
+        try {
+            JsonObject result = HJ.get(
+                    "https://api.ip.sb/geoip/" + ip,
+                    null,
+                    6000,
+                    10000
+            );
+
+            return new IpInfo(
+                    JU.getString(result, "ip", null),
+                    JU.getString(result, "country", null),
+                    JU.getString(result, "country_code", null),
+                    JU.getString(result, "region", null),
+                    JU.getString(result, "region_code", null),
+                    JU.getString(result, "city", null),
+                    JU.getString(result, "postal_code", null),
+                    JU.getString(result, "latitude", null),
+                    JU.getString(result, "longitude", null),
+                    JU.getString(result, "organization", null),
+                    JU.getString(result, "timezone", null),
+                    JU.getString(result, "asn", null)
+            );
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
+    }
+
+    public record IpInfo(
+            String ip,
+            String country,
+            String countryCode,
+            String region,
+            String regionCode,
+            String city,
+            String postalCode,
+            String latitude,
+            String longitude,
+            String org,
+            String timezone,
+            String asn
+    ) {
+        @Override
+        public @NonNull String toString() {
+            SafeStringBuilder sb = new SafeStringBuilder();
+
+            sb.append("IP 地址 - ").append(ip).ln()
+                    .add("国家/地区：{0}{1?}\n", country,
+                            countryCode == null ? null : " (" + countryCode + ")")
+                    .add("区域：{0}{1?}\n", region,
+                            regionCode == null ? null : " (" + regionCode + ")")
+                    .add("城市：{0}{1?}\n", city,
+                            postalCode == null ? null : " (" + postalCode + ")")
+                    .add("位置：{0}, {1}\n", latitude, longitude)
+                    .add("时区：{0}\n", timezone)
+                    .add("组织：{0}\n", org)
+                    .add("ASN：{0}", asn);
+
+            return sb.toString();
+        }
+    }
+}
