7c7377cfdb9644968012135a89e1608fd36beddc
ClovenBugle <yan_hun233@outlook.com>
2025-12-05 18:19:03 +0800

    feat(WeatherCommand): new weather command (WIP)

---
+++ src/main/java/chiloven/lukosbot2/commands/WeatherCommand.java
@@ -0,0 +1,36 @@
+package chiloven.lukosbot2.commands;
+
+import chiloven.lukosbot2.core.CommandSource;
+import com.mojang.brigadier.CommandDispatcher;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+@Service
+@ConditionalOnProperty(
+        prefix = "lukos.commands.switch",
+        name = "weather",
+        havingValue = "true",
+        matchIfMissing = true
+)
+public class WeatherCommand implements BotCommand {
+
+    @Override
+    public String name() {
+        return "weather";
+    }
+
+    @Override
+    public String description() {
+        return "查询天气";
+    }
+
+    @Override
+    public String usage() {
+        return "";
+    }
+
+    @Override
+    public void register(CommandDispatcher<CommandSource> dispatcher) {
+
+    }
+}
