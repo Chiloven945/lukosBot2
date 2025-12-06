8a9d615aaa02cb54391ff7b399abf44ea8a9e516
ClovenBugle <yan_hun233@outlook.com>
2025-12-06 17:01:50 +0800

    fix(DiceCommand): fix dice sum overflow

--- src/main/java/chiloven/lukosbot2/commands/DiceCommand.java
+++ src/main/java/chiloven/lukosbot2/commands/DiceCommand.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.stream.IntStream;
 
 import static chiloven.lukosbot2.util.brigadier.builder.LiteralArgumentBuilder.literal;
@@ -77,24 +78,29 @@ public class DiceCommand implements BotCommand {
                 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
 
         if (count == 1) {
+            int face = IntStream.range(0, 6)
+                    .filter(i -> faces[i] > 0)
+                    .findFirst()
+                    .orElse(0) + 1;
+
             return """
                     你掷了 1 个骰子。
                     朝上的一面是……%d！
-                    """.formatted(IntStream.range(0, 6)
-                    .filter(i -> faces[i] > 0)
-                    .findFirst()
-                    .orElse(0) + 1);
+                    """.formatted(face);
         } else {
+            BigInteger sum = IntStream.range(0, faces.length)
+                    .mapToObj(i -> BigInteger.valueOf(faces[i])
+                            .multiply(BigInteger.valueOf(i + 1L)))
+                    .reduce(BigInteger.ZERO, BigInteger::add);
+
             return """
                     你掷了 %d 个骰子。
                     其中，1朝上的一面有 %d 个、2有 %d 个、3有 %d 个、4有 %d 个、5有 %d 个、6有 %d 个。
-                    他们合计起来是 %d！
+                    他们合计起来是 %s！
                     """.formatted(
                     count,
                     faces[0], faces[1], faces[2], faces[3], faces[4], faces[5],
-                    IntStream.range(0, faces.length)
-                            .mapToLong(i -> faces[i] * (i + 1L))
-                            .sum()
+                    sum.toString()
             );
         }
     }
