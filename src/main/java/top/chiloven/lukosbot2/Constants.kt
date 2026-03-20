47a82150a79dfccd320f2ace629878487d7a1b37
ClovenBugle <yan_hun233@outlook.com>
2026-03-20 18:07:33 +0800

    ver(0.2.0-SNAPSHOT): bump bot version to 0.2.0-SNAPSHOT

--- src/main/java/top/chiloven/lukosbot2/Constants.kt
+++ src/main/java/top/chiloven/lukosbot2/Constants.kt
@@ -6,7 +6,8 @@ import java.util.*
  * Singleton object for holding application-wide constants.
  */
 object Constants {
-    const val VERSION = "0.1.1-SNAPSHOT"
+
+    const val VERSION = "0.2.0-SNAPSHOT"
     const val APP_NAME = "lukosBot2"
     const val UA = "$APP_NAME/$VERSION"
 
@@ -16,35 +17,23 @@ object Constants {
     )
 
     val springBootVersion: String = getImplVersion("org.springframework.boot.SpringApplication")
-    val tgVersion: String? = getMavenVersion("org.telegram", "telegrambots-client")
+    val tgVersion: String = getMavenVersion("org.telegram", "telegrambots-client")
     val jdaVersion: String = getImplVersion("net.dv8tion.jda.api.JDA")
     val shiroVersion: String = getImplVersion("com.mikuac.shiro.boot.Shiro")
 
-    private fun getImplVersion(className: String): String {
-        return try {
-            val clazz = Class.forName(className)
-            clazz.`package`?.implementationVersion ?: "unknown"
-        } catch (ignored: ClassNotFoundException) {
-            "unknown"
-        }
-    }
+    private fun getImplVersion(className: String): String =
+        runCatching { Class.forName(className).`package`?.implementationVersion }
+            .getOrNull() ?: "unknown"
 
-    private fun getMavenVersion(groupId: String, artifactId: String): String? {
+    @Suppress("SameParameterValue")
+    private fun getMavenVersion(groupId: String, artifactId: String): String {
         val path = "META-INF/maven/$groupId/$artifactId/pom.properties"
 
-        return Optional.ofNullable(Thread.currentThread().contextClassLoader.getResourceAsStream(path))
-            .map { inputStream ->
-                try {
-                    inputStream.use {
-                        val p = Properties()
-                        p.load(it)
-                        p.getProperty("version")
-                    }
-                } catch (e: Exception) {
-                    null
-                }
+        return runCatching {
+            Thread.currentThread().contextClassLoader.getResourceAsStream(path)?.use { input ->
+                Properties().apply { load(input) }.getProperty("version")
             }
-            .filter { it?.isNotBlank() == true }
-            .orElse("unknown")
+        }.getOrNull()?.takeIf { it.isNotBlank() } ?: "unknown"
     }
+
 }
