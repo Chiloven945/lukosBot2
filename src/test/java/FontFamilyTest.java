7d45d3919af574275ff7ea0a3152bf64db55083c
ClovenBugle <yan_hun233@outlook.com>
2026-04-03 20:23:44 +0800

    chore(test): remove tests

--- src/test/java/FontFamilyTest.java
+++
@@ -1,6 +0,0 @@
-import java.awt.*;
-
-void main() {
-    String[] names = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-    Arrays.stream(names).forEach(IO::println);
-}
